f64fcf1ac916e2ac46d3868b94232ff14bb6cb49
CoderPrans <pranavbhaskar17@gmail.com>
2019-02-22 19:55:45 +0530

    caching for offline support and display: standalone

--- Pomodoro/sw.js
+++ Pomodoro/sw.js
@@ -1,4 +1,16 @@
-/** An empty service worker! */
 self.addEventListener('fetch', function(event) {
   /** An empty fetch handler! */
+});
+
+// caching static file and assets
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'pomo_img.png',
+        'tomato.png'
+      ]); 
+    }) 
+  ); 
 });
