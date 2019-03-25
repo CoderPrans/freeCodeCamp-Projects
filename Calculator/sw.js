6d1fea962a0d32e4c7c389f0c1e380e48e02bf13
CoderPrans <pranavbhaskar17@gmail.com>
2019-03-25 10:33:26 +0530

    calculator is PWA

---
+++ Calculator/sw.js
@@ -0,0 +1,42 @@
+// Call Fetch Event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request)) 
+  )
+});
+
+
+const cacheName = 'v1';
+
+const cacheAssets = [
+  '/freeCodeCamp-Projects/Calculator/',
+  'index.html',
+  'calculator-icon.png',
+]
+
+// Cache Install Event
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache){
+      return cache.addAll(cacheAssets)  
+    }).then(() => self.skipWaiting())
+  )
+})
+
+// Cache Activate Event
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.map(function(cache){
+          if(cache !== cacheName){
+            return caches.delete(cache) 
+          } 
+        }) 
+      ) 
+    }) 
+  )  
+})
+
+
+
